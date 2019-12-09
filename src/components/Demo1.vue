99ecf23527c6c271d151e464d34023e97f1bff89
max <marfi241@gmail.com>
2019-12-09 17:18:35 +0800

    feat: 修改一下demo1的排版

--- src/components/Demo1.vue
+++ src/components/Demo1.vue
@@ -1,35 +1,33 @@
 <template>
 <div class="demo1">
   <div>
-    <h1>origin sample data:</h1>
-    <div>{{ sample }}</div>
-  </div>
-  <div>
     <div>
-      sample.testInput: <input type="number" @input="validateNumber" v-model.number="sample.testInput" />
-    </div>
-    <div>
+      <h1>style binding</h1>
       <div>
-        <div>Red <input type="number" v-model.number="RGB.r" /></div>
-        <div>Green <input type="number" v-model.number="RGB.g" /> </div>
-        <div>Blue <input type="number" v-model.number="RGB.b" /> </div>
+        <div>Red<input type="number" v-model.number="RGB.r" /> <input type="range" v-model="RGB.r" :min="0" :max="255"> </div>
+        <div>Green <input type="number" v-model.number="RGB.g" /> <input type="range" v-model="RGB.g" :min="0" :max="255"> </div>
+        <div>Blue <input type="number" v-model.number="RGB.b" /> <input type="range" v-model="RGB.b" :min="0" :max="255"> </div>
       </div>
       {{ rgbStyleValue }}
       <div class="color-field" :style="{ backgroundColor: rgbStyleValue }"></div>
     </div>
+
     <div>
-      <input type="range" v-model="RGB.r" :min="0" :max="255">
-      <input type="range" v-model="RGB.g" :min="0" :max="255">
-      <input type="range" v-model="RGB.b" :min="0" :max="255">
+      <h1>origin sample data:</h1>
+      <div>{{ sample }}</div>
+    </div>
+    <h1>input and computed</h1>
+    <div>
+      sample.testInput: <input type="number" @input="validateNumber" v-model.number="sample.testInput" />
+      <div>
+        <button @click="addTestInput">+ 1</button>
+        <button @click="minusTestInput">- 1</button>
+      </div>
     </div>
     <div>origin number: {{ sample.testInput }}</div>
     <div>double number: {{ doubleNumber }}</div>
     <div>Array({{ sample.testInput }}) sno: {{ snoArrayOftestInput }}</div>
     <div>{{ sample.testInput }}內的質數: {{ primeData }}</div>
-
-    <div v-for="n in snoArrayOftestInput" :key="n">
-      <div>{{ n }}內的質數: {{ generatePrimeData(n) }}</div>
-    </div>
   </div>
 </div>
 </template>
@@ -40,8 +38,8 @@ export default {
   data: () => ({
     sample: {
       testInput: 10,
-      testPoint: [0,0],
-      testArray: [0,1,2,3,4],
+      // testPoint: [0,0],
+      // testArray: [0,1,2,3,4],
     },
     RGB: {
       r: 0,
@@ -76,6 +74,12 @@ export default {
     },
   },
   methods: {
+    addTestInput() {
+      this.sample.testInput += 1;
+    },
+    minusTestInput() {
+      this.sample.testInput -= 1;
+    },
     validateNumber() {
       if (this.sample.testInput > 500) {
         this.sample.testInput = 500;
@@ -102,7 +106,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 .demo1 {
   text-align: left;
 }
@@ -111,5 +115,10 @@ export default {
   height: 100px;
   border: 1px solid black;
   background-color: #fff;
+}
+.rgb-input {
+  width: 30%;
+  display: flex;
+  justify-content: space-around;
 }
 </style>
